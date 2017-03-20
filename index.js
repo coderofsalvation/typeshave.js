1522403d77932337c84df54dd21c631d8d73046a
Leon <info@leon.vankammen.eu>
2017-03-20 13:28:16 +0100

    update to es6

--- index.js
+++ index.js
@@ -1,78 +1,44 @@
-var clone;
+let clone     = o => JSON.parse(JSON.stringify(o));
 
-clone = function(o) {
-  return JSON.parse(JSON.stringify(o));
-};
+module.exports = function() {
 
-module.exports = (function() {
-  var typeshave;
-  typeshave = this;
+  let typeshave = this;
+
   this.tv4 = require('tv4-node').tv4;
-  this.validate = function(data, schema) {
-    var _data, i, k, objdata, v;
-    _data = data;
-    if (data && (data.callee != null)) {
-      _data = Array.prototype.slice.call(data);
+  this.validate = function(data,schema) { 
+    let _data = data;
+    if (data && (data.callee != null)) { _data = Array.prototype.slice.call(data); }
+    if (!(schema.type != null)) { // for inline function wrappers only (simple args and not phat object)
+      let objdata = {}; let i=1; for (let k in schema) { let v = schema[k];       objdata[k] = data[i++];  _data = objdata; }
+      schema = { type: "object", required: Object.keys(schema),  properties: schema };
     }
-    if (schema.type == null) {
-      objdata = {};
-      i = 1;
-      for (k in schema) {
-        v = schema[k];
-        objdata[k] = data[i++];
-      }
-      _data = objdata;
-      schema = {
-        type: "object",
-        required: Object.keys(schema),
-        properties: schema
-      };
-    }
-    return typeshave.tv4.validate(_data, schema);
+    return typeshave.tv4.validate(_data, schema); 
   };
+
   this.verbose = 0;
-  this.onError = (function(_this) {
-    return function(err) {
-      if (_this.verbose > 0) {
-        console.log(JSON.stringify({
-          'typeshave exception': err
-        }, null, 2));
-      }
-      throw "TYPESAFE_FAIL";
-    };
-  })(this);
-  this.typesafe = function(schema, method) {
-    var validated;
-    validated = function() {
-      var _schema, args, dump, i, k, v;
-      args = {};
-      _schema = clone(schema);
-      if (_schema.type == null) {
-        i = 0;
-        for (k in schema) {
-          v = schema[k];
-          args[k] = arguments[i++];
-        }
-        _schema = {
-          type: "object",
-          required: Object.keys(schema),
-          properties: schema
-        };
-      } else {
-        args = arguments[0];
-      }
-      v = typeshave.validate(args, _schema);
+
+  this.onError = err => {
+    if (this.verbose > 0) { console.log(JSON.stringify({'typeshave exception': err},null,2)); }
+    throw new Error("TYPESAFE_FAIL");
+  };
+
+  this.typesafe = function(schema,method) {
+    let validated = function() {
+      let args = {};  let _schema = clone(schema);
+      if (!(_schema.type != null)) { // for inline function wrappers only (simple args and not phat object)
+        let i=0; for (let k in schema) { var v = schema[k];         args[k] = arguments[i++]; }
+        _schema = { type: "object", required: Object.keys(schema),  properties: schema };
+      } else { args = arguments[0]; }
+      var v = typeshave.validate(args, _schema);
       if (!v) {
-        dump = {
-          data: args,
-          errors: v.error,
-          schema: _schema
-        };
-        typeshave.onError(dump);
+        let dump = { data: args, errors: v.error, schema: _schema };
+        throw new Error(dump);
       }
       return method.apply(this, arguments);
     };
     return validated;
   };
+
   return this;
-}).apply({});
+
+}.apply({});
